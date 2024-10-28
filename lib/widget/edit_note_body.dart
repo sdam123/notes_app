cd051fd66dc77e50fcafc60e32808b702d31ae74
sdam123 <alzamlsdam888@gmail.com>
2024-10-28 15:41:09 +0300

    finsh edit screen

--- lib/widget/edit_note_body.dart
+++ lib/widget/edit_note_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widget/custom_appbar.dart';
+import 'package:note_app/widget/custom_text_field.dart';
 
 class EditNoteBody extends StatelessWidget {
   const EditNoteBody({super.key});
@@ -8,16 +9,29 @@ class EditNoteBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: const Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          CustomAppbar(
-            title: 'Edit Notes',
-            icon: Icons.check,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            CustomAppbar(
+              title: 'Edit Notes',
+              icon: Icons.check,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextfield(hint: 'title'),
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextfield(
+              hint: 'content',
+              maxline: 15,
+            )
+          ],
+        ),
       ),
     );
   }
